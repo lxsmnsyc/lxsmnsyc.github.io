b94b11b7e581f71ca2ae5486a268a091e9def494
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-06 13:50:07 +0800

    Create precache-manifest.eb9c1eebc702897fad52b10a4054db58.js

---
+++ precache-manifest.eb9c1eebc702897fad52b10a4054db58.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c342813855b00e11219c11801850944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6c920d1e3884b629c40",
+    "url": "/static/css/main.43b512cd.chunk.css"
+  },
+  {
+    "revision": "251255d8d0630308b8a1",
+    "url": "/static/js/2.aec4b3ca.chunk.js"
+  },
+  {
+    "revision": "c6c920d1e3884b629c40",
+    "url": "/static/js/main.41b0da90.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
