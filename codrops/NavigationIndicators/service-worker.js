674fc36a0f227f83ad87be5d1ef3e708b0a94b6e
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-06-27 17:40:42 +0800

    NavigationIndicators

--- codrops/NavigationIndicators/service-worker.js
+++ codrops/NavigationIndicators/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8bec35e50c6613c4d99013828598954b.js"
+  "/codrops/NavigationIndicators/precache-manifest.32f50241a036083495fc63afe065a4b4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/codrops/NavigationIndicators/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
